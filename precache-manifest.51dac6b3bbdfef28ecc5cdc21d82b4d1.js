065ddb90a3bc5e3a133270c4b77a3935e666dd91
Edward <e.arekhva@gmail.com>
2020-10-04 19:18:53 +0200

    Updates

---
+++ precache-manifest.51dac6b3bbdfef28ecc5cdc21d82b4d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b0f5cf3f92e99a82971ab7467b19b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f1eb65b31f71af85d16",
+    "url": "/static/css/main.69a6d024.chunk.css"
+  },
+  {
+    "revision": "8a2025f970e6eedfec82",
+    "url": "/static/js/2.57700623.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57700623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1eb65b31f71af85d16",
+    "url": "/static/js/main.c57d6b8e.chunk.js"
+  },
+  {
+    "revision": "262305fd8ff8fe0b82eb",
+    "url": "/static/js/runtime-main.a2008f93.js"
+  }
+]);
